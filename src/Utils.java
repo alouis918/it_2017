1d0597b2148b3280299fc7f119ae1177b33571d2
Desmond Noutcha <desmond@distribution-manager.org>
2017-07-27 11:09:22 +0200

    canvas line drawer ready for use

---
+++ src/Utils.java
@@ -0,0 +1,15 @@
+public class Utils {
+
+    public static String login(String username, String password) {
+        if (username.equalsIgnoreCase("louis14") && password.equalsIgnoreCase("freddylebouc")){
+            return  "{result:true}";
+        }else {
+            return  "{result:false}";
+        }
+    }
+
+    public static void SaveCanvas(String canvas) {
+        
+    }
+
+}
