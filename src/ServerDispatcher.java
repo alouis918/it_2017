cad86937e17aaeeb6a776fd553277b762becf322
Freddy <aresschneider@gmx.ch>
2017-07-29 09:16:13 +0200

    cleaning up werverDispatcher

--- src/ServerDispatcher.java
+++ src/ServerDispatcher.java
@@ -21,7 +21,7 @@ public class ServerDispatcher  extends Thread
 	String rootPath="src\\serverDirectory";
 	BufferedInputStream bis=null;
 	FileInputStream fis=null;
-	byte [] byteArray;
+	//byte [] byteArray;
 	File file=null;
 
 	/*
@@ -31,7 +31,7 @@ public class ServerDispatcher  extends Thread
 	{
 		try {
 			file=new File(FileName);
-			byteArray=new byte[(int)file.length()];
+			//byteArray=new byte[(int)file.length()];
 			return  new FileInputStream(file);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -40,65 +40,7 @@ public class ServerDispatcher  extends Thread
 		}
 		return null;
 }
-	/**
-	 * 
-	 * @param fis get file InputStream pointing to a 
-	 * @return
-	 */
-	String [] getFileNameFromPath()
-	{
-		String [] array = null;
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			String inputLine;
-			int a = 0;
-			String []str = null;
-			String temp="";
-		
-			int i =0;
-			
-//			while ((str = in.readLine()) != null) {
-//			      System.out.println(i+++str);
-//			    }
-//			while(true){
-//				temp= in.readLine();
-//				System.out.println("i= "+i+++" "+temp.length());	
-//				if (temp.length() == 0)					
-//				{
-//					break;
-//				}
-//				else 
-//					{
-//					str+=temp;
-//					//System.out.println(a+":"+str);
-//					}				
-//				}
-			//System.out.println("hihihihihih");
-			//System.out.println("str = " + str);
-			//inputLine = in.readLine();
-			
-			//inputLine=str;
-			//array=inputLine.split(" ");   
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return array;
-	}
-
-	String[] postResponse(){
-		String[] array = {};
-		try {
-			BufferedReader inBuffer = new BufferedReader(new InputStreamReader(in));
-			String inputLine;
-			inputLine = inBuffer.readLine();
-			array = inputLine.split("\n");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return  array;
-	}
+	
 	/**
 	 * parse a file name given in the form \filename
 	 * @param fileName Name of the file to be returned
@@ -133,50 +75,21 @@ public class ServerDispatcher  extends Thread
 		int len;
 		PrintWriter print = new PrintWriter(out);
 		BufferedReader infile = new BufferedReader(new InputStreamReader(fis));
-		System.out.println("infile"+infile.readLine());
-//		while((len=fis.read(byteArray))!=-1)
-//		{
-//		}
+		String fromFile = "",temp ;
+		while((temp = infile.readLine()) != null)
+		{
+		   fromFile += temp; 
+		}
+		//System.out.println("infile"+fromFile);
+		
 		String response1 = "HTTP/1.1 200 OK\r\n" +
-			    "Content-Length: 22\r\n" +
-			    "Content-Type: text/html\r\n\r\n";
-			    //+"<h1>hihih</h1>";
-		String response = "";
-		System.out.println("bytearray1"+byteArray.toString());
+			    "Content-Length:"+ fromFile.length()+"\r\n" +
+			    "Content-Type: text/html\r\n\r\n"; 
+		print.println(response1+fromFile);
 		
-		for (byte b : byteArray) {
-			response+=(char)b;
-		}
-		System.out.println("bytearray2"+byteArray[0]);
-		System.out.println("response="+response);
-		System.out.println("response+response1="+response1+response);
-		
-		print.println(response1+response);
-		/*print.println(response1);
-		print.flush();
-		print.println();
-		String response = "";
-		for (byte b : byteArray) {
-			response+=(char)b;
-		}
-		print.println(response);*/
 		print.flush();
 		print.close();
 		//out.flush();
-	}
-
-	/**
-	 * 
-	 * @param fis
-	 * @throws IOException
-	 */
-	void sendFIleOverSOcketBuffered(FileInputStream fis) throws IOException
-	{
-		//Writing/sending the file  to the client passing via a buffer
-		bis= new BufferedInputStream(fis);
-		bis.read(byteArray,0,byteArray.length);
-		out.write(byteArray,0,byteArray.length);
-		out.flush();
 	}
 
 	/**
@@ -204,6 +117,7 @@ public class ServerDispatcher  extends Thread
 			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			System.out.println("Connection established with"+clientSocket.toString());
 			String [] str = in.readLine().split(" ");
+			
 			if(str[0].equalsIgnoreCase("GET"))
 			{
 				 FileInputStream fileStream = parseFileName(str[1]);
@@ -214,35 +128,34 @@ public class ServerDispatcher  extends Thread
 			}
 			else
 			{
-				
+			    String temp,fromFile = "";
+		        while((temp = in.readLine()) != null)
+		        {
+		            System.out.println(temp);
+		            fromFile += temp;
+		            if (temp.length() == 0)
+		            {
+		                int tempInt;
+		                while((tempInt = in.read())!= -1)
+		                {
+		                    char ch = (char)tempInt;
+		                    System.out.println(ch);
+		                }
+		                System.out.println("out of while loop");
+		                break;
+		            }
+		         }
+		        System.out.println("out");
+		       // System.out.println("out");
+		       //in.readLine();in.readLine();in.readLine();
+		        //System.out.println("post fromfile: "+ fromFile+in.readLine());
+		        //System.out.println(builder.toString());
 			}
 			
-			/*System.out.println("Connection established with"+clientSocket.toString());
-            String[] httpHeader = getFileNameFromPath();
-            for (String string : httpHeader) {
-				
-            	System.out.print(string);
-			}
-            if(httpHeader[0].equalsIgnoreCase("GET")){ // case where is a get request
-                FileInputStream fileStream = parseFileName(httpHeader[1]);
-                out.write("\n".getBytes());
-                if ( fileStream == null){
-                    return;
-                }
-                getResponse(fileStream);
-                //sendFIleInfoWithJSON();
-            }else if (httpHeader[0].equalsIgnoreCase("POST")){ // case where it is a post request
-                 //System.out.println("hihih"+Arrays.binarySearch(httpHeader,1,httpHeader.length,"\r\n")+1);
-                 System.out.println(httpHeader[9]);
-				String [] str = postResponse();
-				System.out.println( str[1]);
-            }
-*/
+			
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			try {
@@ -251,7 +164,6 @@ public class ServerDispatcher  extends Thread
 				if(out!=null)out.close();
 				if(in!=null) in.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
